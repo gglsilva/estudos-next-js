68e42bda05f6ddfe14c7cba6f586006831cefdd6
gglsilva <gabrielchamex@gmail.com>
2023-12-20 10:15:16 -0300

    Add navegação entre paginas

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,11 @@
+import Link from "next/link"
 export default function About(){
-    return <h1>Página de About</h1>
+    return(
+        <>
+            <h1>Página de About</h1>
+            <Link href="/">
+                <p>Voltar para home</p>
+            </Link>
+        </>
+    ) 
 }
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,3 +1,11 @@
+import Link from "next/link"
 export default function Products(){
-    return <h1>Página de Produtos</h1>
+    return (
+        <>
+            <h1>Página de Produtos</h1>
+            <Link href="/">
+                <p>Voltar para home</p>
+            </Link>
+        </>
+    )
 }
