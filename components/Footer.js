aaaa234bf1001ae1253aaba0d35acaa2ee15916c
gglsilva <gabrielchamex@gmail.com>
2023-12-21 15:51:32 -0300

    add css

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import Styles from '../styles/Footer.module.css'
+export default function Footer(){
+    return (
+        <>
+            <div className={Styles.footer} >
+                <p>Curso de Next.Js &copy; 2023</p>
+            </div>
+        </>
+    )
+}
---
+++ components/MainConteiner.js
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function MainContainer({children}){
+
+  return(
+    <>
+      <Navbar />
+      <div>
+        {children}
+      </div>
+      <Footer />
+    </>
+  )
+}
