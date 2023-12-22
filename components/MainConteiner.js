ac6a00b86eb622a324c11d0ca0c63ecb4fe38f0f
gglsilva <gabrielchamex@gmail.com>
2023-12-22 16:33:07 -0300

    css com next

--- components/MainConteiner.js
+++ components/MainConteiner.js
@@ -1,12 +1,14 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
+import Style from '../styles/MainContainer.module.css'
+
 export default function MainContainer({children}){
 
   return(
     <>
       <Navbar />
-      <div>
+      <div className={Style.container}>
         {children}
       </div>
       <Footer />
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import { Inter } from 'next/font/google'
-
+import Style from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World Next.js</h1>
+      <h1 className={Style.title}>Hello World Next.js</h1>
     </>
   )
 }
